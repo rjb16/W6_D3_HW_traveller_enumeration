d81f1d9029415a63a70d041d1d35518bf6de13ee
Ruben Botha <rubenjohannesbotha@gmail.com>
2020-07-22 18:29:49 +0100

    journey by transport passes

---
+++ models/traveller.js
@@ -0,0 +1,43 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  const result = this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
+  return result;
+
+  };
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  const result = this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+  return result;
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  const result = this.journeys.filter((journey) => {
+    if (journey.transport === transport) {
+      return journey;
+    }
+  });
+  return result;
+
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
