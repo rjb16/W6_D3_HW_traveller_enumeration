433d2fd4f82c9906dbbe35a42c1a17d001c5f755
Ruben Botha <rubenjohannesbotha@gmail.com>
2020-07-23 09:42:02 +0100

    extensions completed, refactored

--- models/traveller.js
+++ models/traveller.js
@@ -3,50 +3,42 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-  const result = this.journeys.map((journey) => {
+  return this.journeys.map((journey) => {
     return journey.startLocation;
   });
-  return result;
-
-  };
+};
 
 Traveller.prototype.getJourneyEndLocations = function () {
-  const result = this.journeys.map((journey) => {
+  return this.journeys.map((journey) => {
     return journey.endLocation;
   });
-  return result;
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-  const result = this.journeys.filter((journey) => {
-    if (journey.transport === transport) {
-      return journey;
-    }
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
   });
-  return result;
-
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-  const result = this.journeys.filter((journey) => {
-    if (journey.distance >= minDistance){
-      return journey
-    }
+  return this.journeys.filter((journey) => {
+    return journey.distance > minDistance;
   });
-  return result;
-
 };
 
-Traveller.prototype.calculateTotalDistanceTravelled = function (journeys) {
-  const result = this.journeys.reduce((totalDistance, journey) => {
-    return totalDistance + journey.distance;
-  }, 0)
-  return result;
-
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total + journey.distance;
+  }, 0);
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  return this.journeys.map((journey) => {
+    return journey.transport;
+  })
+  .filter((transport, index, array) => {
+    return array.indexOf(transport) === index;
+  });
 };
 
 
